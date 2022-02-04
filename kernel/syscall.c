438c87a74f506fa8be083f5a932e9e438b5fbd1f
luli <luli@tsign.cn>
2022-02-04 09:41:28 +0800

    trace lab

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -104,6 +104,7 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_wait(void);
 extern uint64 sys_write(void);
 extern uint64 sys_uptime(void);
+extern uint64 sys_trace(void);
 
 static uint64 (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -127,6 +128,7 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_trace]   sys_trace,
 };
 
 void
@@ -137,7 +139,13 @@ syscall(void)
 
   num = p->trapframe->a7;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
+    // sys_func func = syscalls[num];
     p->trapframe->a0 = syscalls[num]();
+    if (p->trace_mask)
+    {
+      printf("%d: syscall %d-> %d\n", p->pid, num, p->trapframe->a0);
+    }
+    
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
